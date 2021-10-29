7e348040822c4d7e5bf41c93a0369db3349cbc0f
JustinLee <justinleeyile@gmail.com>
2021-10-29 16:04:13 +0800

    typo

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use("/api/bookingTable", bookingTable);
 // app.use("/api/nurse", nurse);
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
+  res.sendFile(path.join(__dirname, "./frontend/build", "index.html"));
 });
 
 // LISTEN
